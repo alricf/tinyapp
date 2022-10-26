1d8a598e61f6b11fd0e21ae9799f339910d0630a
ACF <alricfernandes11@gmail.com>
2022-10-26 14:23:47 -0400

    Adding a second route and template

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 // Express templating engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // Database
 const urlDatabase = {
@@ -30,6 +30,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 // Port listen handle / Connection checker
