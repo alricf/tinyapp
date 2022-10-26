016aaecf42f2382dc110d474ff65867f9fb72f96
ACF <alricfernandes11@gmail.com>
2022-10-26 12:44:02 -0400

    views folder added and urlDatabase object sent to it

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 // Port listen handle / Connection checker
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
